fa4074e4b96aa756d4534e47bdfda1c2ecfb13df
Raku Saito <tofu@wide.ad.jp>
2023-08-21 20:24:57 +0900

    day 1

--- c-test3/main.c
+++ c-test3/main.c
@@ -19,7 +19,6 @@ struct segment_routing_header {
     uint8_t last_entry;   // 最後のエントリーのインデックス
     uint8_t flags;        // フラグフィールド
     uint16_t tag;         // タグ (オプション)
-    uint32_t reserved;    // 予約済みフィールド
     uint32_t segments[MAX_SEGMENTS]; // セグメント識別子のリスト
 };
 
@@ -44,6 +43,7 @@ int main(){
     char packet[1024];
     // 中身を0で初期化する
     memset(packet, 0, 1024);
+    
     // ipv6ヘッダーの構造体のポインタ = packetの先頭アドレス (packetにipv6ヘッダーの構造体を被せる=キャストする)
     struct ip6_hdr *ip6_header = (struct ip6_hdr *) packet;
     // "->" : アロー演算子 (構造体の中身にアクセスする) 
@@ -52,13 +52,36 @@ int main(){
     // inet_pton() : ドット形式のIPv4アドレスまたはIPv6アドレスをバイナリ形式に変換する
     ip6_header->ip6_flow = htonl((6 << 28) | (0 << 20) | 0);
     ip6_header->ip6_plen = htons(1024 - sizeof(struct ip6_hdr));
-    ip6_header->ip6_nxt = IPPROTO_ICMPV6;
+    ip6_header->ip6_nxt = IPPROTO_ROUTING;
     ip6_header->ip6_hops = 255;
     inet_pton(AF_INET6, "::1", &(ip6_header->ip6_src));
-    inet_pton(AF_INET6, "::1", &(ip6_header->ip6_dst));
+    // SRでは、Active Segmentがdest addrになる
+    // inet_pton(AF_INET6, "::1", &(ip6_header->ip6_dst));
+
+    // srヘッダーの構造体のポインタ = packetの先頭アドレス + ipv6ヘッダーのサイズ (packetにsrヘッダーの構造体を被せる=キャストする)
+    struct segment_routing_header *sr_header = (struct segment_routing_header *) (packet + sizeof(struct ip6_hdr));
+    // "->" : アロー演算子 (構造体の中身にアクセスする)
+    sr_header->next_header = IPPROTO_ICMPV6;
+    // SRHの場合は4
+    sr_header->routing_type = 4;
+    // セグメントの何番目を使う？
+    sr_header->segments_left = 0;
+    // セグメントの最後の番号 (0から数える)
+    sr_header->last_entry = 1;
+    // 特殊用途だけど普通は0らしい
+    sr_header->flags = 0;
+    sr_header->tag = 0;
+    inet_pton(AF_INET6, "1::1", &(sr_header->segments[0]));
+    inet_pton(AF_INET6, "1::1", &(ip6_header->ip6_dst));
+    // 型が違ってできない　ほんとうはこうやりたい
+    // ip6_header->ip6_dst = sr_header->segments[0];
+    inet_pton(AF_INET6, "1::2", &(sr_header->segments[1]));
+    sr_header->hdr_ext_len = (sizeof(sr_header) - 8) / 8;
+    
+    
 
     // パケットのペイロード部分(packetに皮を被せる)
-    char *payload = packet + sizeof(struct ip6_hdr);
+    char *payload = packet + sizeof(struct ip6_hdr) + sizeof(struct segment_routing_header);
     // strcpy() : 文字列をバイナリにして代入する
     strcpy(payload, "Hello, World!");
 
