291f1682e441d457e3baca5edfb206dde5fd1acb
tofu <tofu@wide.ad.jp>
2024-03-02 19:10:30 +0900

    upd check

---
+++ 00_note/memo.md
@@ -0,0 +1,9 @@
+ヘッダーサイズ
+```
+Type: byte/bit
+---
+ether: 14/112
+ipv6: 40/320
+srv6 header (SID含まず): 8/64
+sid: 16/128 /1sid
+```
--- 02_rawsock/recv.c
+++ 02_rawsock/recv.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <linux/if_ether.h>
+#include <linux/if_ether.h> // for ETH_P_ALL
+
+int mac_check(int offset, unsigned char *buf, unsigned char *mac){
+    for(int i=0; i<6; i++){
+        if (buf[offset+i] != mac[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void packet_processing(unsigned char *buf, ssize_t len){
+    // wan-side nic mac addr
+    unsigned char node_mac_dst[6] = {0x9e, 0xd0, 0x9e, 0x53, 0x3d, 0xb2};
+    // if dst or src mac is not for me, return
+    if (mac_check(0, buf, node_mac_dst) == 0
+        && mac_check(6, buf, node_mac_dst) == 0) return;
+
+    // ethertype is not 0x86dd, return
+    if (buf[12] != 0x86 || buf[13] != 0xdd) return;
+    // ipv6 next header is not 43 (routing header), return
+    if (buf[20] != 43) return;
+
+    printf("Received SRv6 Packet! (%ld bytes)\n", len);
+    for(int i=0; i<len; i++){
+        printf("%02x ", buf[i]);
+    }
+    printf("\n\n");
+}
 
 int main(void){
     int sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
@@ -16,13 +44,8 @@ int main(void){
         if (len < 0) {
             fprintf(stderr, "Failed to receive\n");
             return 1;
-        }
-        if (len!=0){
-            printf("Received %ld bytes\n", len);
-            for(int i=0; i<len; i++){
-                printf("%02x ", buf[i]);
-            }
-            printf("\n\n");
+        }else if (len!=0){
+            packet_processing(buf, len);
         }
     }
 }
