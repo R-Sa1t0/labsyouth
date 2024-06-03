276f3979cea88dbd7d835d3d0731de773d2e85db
tofu <tofu@wide.ad.jp>
2024-06-03 12:57:28 +0900

    encap.c : ok?

--- 02_rawsock/encap.c
+++ 02_rawsock/encap.c
@@ -6,7 +6,9 @@
 #include <unistd.h>  //close()
 
 #include <linux/in6.h>  //struct in6_addr
-#include <net/ethernet.h>  //struct ether_addr
+#include <net/ethernet.h>  //struct ether_addr, struct eth_header
+#include <netinet/ip6.h> // struct ip6_hdr
+#include <linux/seg6.h> // srv6 hdr
 #include <arpa/inet.h>  //inet_pton()
 #include <sys/ioctl.h>  //ioctl()
 #include <net/if.h>  //struct ifreq
@@ -35,7 +37,7 @@ void buffer_print(Buffer *buf){
     puts("");
 }
 
-struct ether_addr get_macaddr(char *nic_name){
+struct ether_addr get_macaddr(const char *nic_name){
     struct ifreq ifr;
     struct ether_addr addr;
 
@@ -60,6 +62,17 @@ struct ether_addr get_macaddr(char *nic_name){
     return addr; 
 }
 
+int parse_macaddr(struct ether_addr *addr,const char *addr_str){
+    if (sscanf(addr_str, "%x:%x:%x:%x:%x:%x", 
+        addr->ether_addr_octet[0], addr->ether_addr_octet[1],
+        addr->ether_addr_octet[2], addr->ether_addr_octet[3],
+        addr->ether_addr_octet[4], addr->ether_addr_octet[5])==6){
+    }else{
+        return -1;
+    }
+    return 1;
+}
+
 typedef struct {
     struct in6_addr ip6_srcaddr;
     struct in6_addr ip6_dstaddr;
@@ -67,11 +80,12 @@ typedef struct {
     char *wan_nic_name;
     struct ether_addr lan_mac;
     struct ether_addr wan_mac;
+    struct ether_addr dst_mac;
 }arg;
 arg parse_arg(int argc, char *argv[]){
     arg result;
-    if (argc != 5) {
-        fprintf(stderr, "Usage: %s [srcaddr] [dstaddr] [lan-side-nic-name] [wan-side-nic-name]\n", argv[0]);
+    if (argc != 6) {
+        fprintf(stderr, "Usage: %s [srcaddr] [dstaddr] [lan-side-nic-name] [wan-side-nic-name] [dst mac addr]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     if (inet_pton(AF_INET6, argv[1], &result.ip6_srcaddr) != 1) {
@@ -90,6 +104,10 @@ arg parse_arg(int argc, char *argv[]){
     result.wan_nic_name = strdup(argv[4]);
     if (result.wan_nic_name == NULL) {
         perror("strdup");
+        exit(EXIT_FAILURE);
+    }
+    if ((parse_macaddr(&result.dst_mac, argv[5]))!=1) {
+        fprintf(stderr, "Invalid destination mac address: %s\n", argv[5]);
         exit(EXIT_FAILURE);
     }
     result.lan_mac = get_macaddr(result.lan_nic_name);
@@ -131,6 +149,38 @@ int open_pernic_rawsock(char* nic_name){
 Buffer do_seg6encap(arg config, Buffer* payload){
     Buffer tmpbuf;
     buffer_init(&tmpbuf);
+    
+    struct ether_header eth_hdr = {
+        config.dst_mac.ether_addr_octet, // dst
+        config.lan_mac.ether_addr_octet, // src
+        htons(ETHERTYPE_IPV6) // ethertype
+    };
+    buffer_append(&tmpbuf,(uint8_t *)&eth_hdr, sizeof(eth_hdr));
+
+    struct ip6_hdr ipv6_hdr= {
+        .ip6_flow = htonl(0x60000000),
+        .ip6_plen = htons(72),
+        .ip6_nxt = IPPROTO_ROUTING,
+        .ip6_hlim = 63,
+        .ip6_src= config.ip6_srcaddr,
+        .ip6_dst= config.ip6_dstaddr
+    };
+    buffer_append(&tmpbuf,(uint8_t *)&ipv6_hdr, sizeof(struct ip6_hdr));
+
+    struct ipv6_sr_hdr *srh = malloc(sizeof(struct ipv6_sr_hdr) + sizeof(struct in6_addr));
+    memset(srh, 0, sizeof(struct ipv6_sr_hdr) + sizeof(struct in6_addr));
+    srh->nexthdr = 0x8f; // Ethernet!
+    srh->hdrlen = 0x02;
+    srh->type = 0x04;
+    srh->segments_left = 0x00;
+    srh->first_segment = 0x00;
+    srh->flags = 0x00;
+    srh->tag = 0x00;
+    memcpy(&srh->segments[0], &config.ip6_dstaddr, sizeof(struct in6_addr));
+    buffer_append(&tmpbuf,(uint8_t *)srh, sizeof(struct ipv6_sr_hdr) + sizeof(struct in6_addr));
+
+    buffer_append(&tmpbuf, payload->v, payload->len);
+
     return tmpbuf;
 }
 
@@ -139,15 +189,33 @@ int main(int argc, char *argv[]){
     int lan_fd = open_pernic_rawsock(config.lan_nic_name);
     int wan_fd = open_pernic_rawsock(config.wan_nic_name);
 
+    struct sockaddr_ll dst_addr;
+    memset(&dst_addr, 0, sizeof(dst_addr));
+    dst_addr.sll_family = AF_PACKET;
+    dst_addr.sll_protocol = htons(ETH_P_ALL);
+    dst_addr.sll_ifindex = if_nametoindex(config.wan_nic_name);
+    dst_addr.sll_halen = ETH_ALEN;
+    memcpy(dst_addr.sll_addr, config.dst_mac.ether_addr_octet, ETH_ALEN);
+
     Buffer rcvbuf;
     buffer_init(&rcvbuf);
+    Buffer sendbuf;
     while (1){
         ssize_t len = recv(lan_fd, rcvbuf.v, rcvbuf.len, 0);
         if (len < 0) {
             fprintf(stderr, "Failed to receive\n");
             return 1;
         }else if (len!=0){
+            printf("payload: ");
             buffer_print(&rcvbuf);
+            sendbuf = do_seg6encap(config, &rcvbuf);
+            int err = sendto(wan_fd, sendbuf.v, sendbuf.len, 0, (struct sockaddr *)&dst_addr, sizeof(dst_addr));
+            if (err < 0) {
+                perror("sendto");
+                close(wan_fd);
+                exit(EXIT_FAILURE);
+            }
+            printf("SRv6 packet sent successfully.\n");
         }
     }
 }
