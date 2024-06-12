3cb3323d8103fffdf2787ba7156a683f1398ed82
tofu <tofu@wide.ad.jp>
2024-06-12 13:45:49 +0900

    buf.c&buf.h : fix

--- 02_rawsock/lib/buf.c
+++ 02_rawsock/lib/buf.c
@@ -12,7 +12,7 @@ int buffer_init(Buffer *buf){
 }
 int (*buffer_clear)() = buffer_init; //alias
 
-int buffer_append(Buffer *buf, const uint8_t *data, size_t data_size){
+int buffer_append(Buffer *buf, const uint8_t *data, const size_t data_size){
     if (buf->len+data_size>BUFFER_SIZE) return -1; // buffer full
     memcpy(buf->v+buf->len, data, data_size);
     if (buf->v==NULL) return -1; // memcpy err
--- 02_rawsock/lib/buf.h
+++ 02_rawsock/lib/buf.h
@@ -12,5 +12,5 @@ typedef struct{
 }Buffer;
 
 int buffer_init(Buffer *buf);
-int buffer_append(Buffer *buf, const uint8_t *data, size_t data_size);
+int buffer_append(Buffer *buf, const uint8_t *data, const size_t data_size);
 void buffer_print(const Buffer *buf);
