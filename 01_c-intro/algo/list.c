9c1c58089ca0644d49658ee423fc6886a9c50160
tofu <tofu@wide.ad.jp>
2024-08-05 11:28:50 +0900

    OK

--- 01_c-intro/algo/list.c
+++ 01_c-intro/algo/list.c
@@ -1,21 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-typedef int data_t;
+typedef uint8_t data_t;
 struct cell{
     data_t data;
     struct cell *next;
 };
 
-static void insert(struct cell* cell_ptr, data_t newval)
+static struct cell * insert(struct cell* cell_ptr, data_t newval)
 {
     struct cell *new_cell=malloc(sizeof(struct cell));
-    if(new_cell==NULL) return;
+    if(new_cell==NULL) puts("malloc error!");
 
     new_cell->data=newval;
     new_cell->next=NULL;
     cell_ptr->next=new_cell;
+
+    return new_cell;
+}
+
+static struct cell * delete(struct cell* cell_ptr)
+{
+    if(cell_ptr==NULL) return NULL;
+
+    struct cell *next_cell = cell_ptr->next;
+    free(cell_ptr);
+
+    return next_cell;
+}
+static void delete2(struct cell* head, uint8_t n)
+{
+    struct cell *p = head;
+    struct cell *p_old = NULL;
+
+    for (uint8_t i = 0; i < n; i++) {
+        if (p == NULL) return;
+        p_old = p;
+        p = p->next;
+    }
+
+    p_old->next = delete(p);
 }
 
 static void print(struct cell *head_ptr)
@@ -33,12 +59,22 @@ static void print(struct cell *head_ptr)
 
 int main(void)
 {
-    struct cell c1,*head;
+    struct cell c1,*head,*last;
     head=&c1;
     c1.data=100;
     c1.next=NULL;
     print(head);
 
-    insert(&c1, 200);
+    last=insert(&c1, 200);
+    print(head);
+    print(last);
+
+    for(uint8_t i=0;i<10;i++) last=insert(last,i);
+    print(head);
+
+    head->next = delete(head->next);
+    print(head);
+
+    delete2(head, 3);
     print(head);
 }
