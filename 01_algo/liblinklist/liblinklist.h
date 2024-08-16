ddefd17f9e240949c3ee8aba2bd7253395263b70
tofu <tofu@wide.ad.jp>
2024-08-16 16:07:17 +0900

    use clang-format

--- 01_algo/liblinklist/liblinklist.h
+++ 01_algo/liblinklist/liblinklist.h
@@ -1,41 +1,47 @@
-#pragma onece
+#pragma once
 
-//#ifndef LIBLINKLIST_H
-//#define LIBLINKLIST_H
-
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef uint32_t data_t;
-// 二重リスト
-typedef struct cell{
+
+// Node
+typedef struct cell {
     data_t data;
-    struct cell *next;
-}Cell;
-typedef struct list{
+    struct cell* next;
+} Cell;
+typedef struct list {
     Cell* head;
     Cell* tail;
     size_t n;
-}List;
+} List;
 
+// boolにする
+uint8_t
+display_list(List* l);
 
-uint8_t display_list(List* l);
-
-List* init_list();
-//uint8_t delete_list(List* l);
+List*
+init_list();
+// uint8_t delete_list(List* l);
 
 // n番目のポインタ (nは0から)
-Cell* seek(List* l, size_t n);
+Cell*
+seek(List* l, size_t n);
 
-uint8_t append_cell(List* l, data_t v);
-uint8_t insert_cell(List* l, Cell* c, data_t v);
+uint8_t
+append_cell(List* l, data_t v);
+uint8_t
+insert_cell(List* l, Cell* c, data_t v);
 
-uint8_t delete_headcell(List* l);
-uint8_t delete_tailcell(List* l);
-uint8_t delete_nextcell(List* l, Cell* c);
-uint8_t delete_cell(List* l, Cell* c);
-uint8_t delete_list(List* l);
-
-//#endif
+uint8_t
+delete_headcell(List* l);
+uint8_t
+delete_tailcell(List* l);
+uint8_t
+delete_nextcell(List* l, Cell* c);
+uint8_t
+delete_cell(List* l, Cell* c);
+uint8_t
+delete_list(List* l);
--- 01_algo/liblinklist/linklist_test.c
+++ 01_algo/liblinklist/linklist_test.c
@@ -3,7 +3,6 @@
 
 #include "liblinklist.h"
 
-
 //エラー時のスタックトレースを取りたい
 void
 errExit(const char* msg)
@@ -17,51 +16,57 @@ warn(const char* msg)
     printf("WARN: %s\n", msg);
 }
 
-
 int
 main(void)
 {
     printf("init\n");
     List* l = init_list();
-    if (l == NULL) exit(1);
+    if (l == NULL)
+        exit(1);
     display_list(l);
 
     printf("append x5\n");
-    for (int32_t i=0; i<5; i++) {
-        if ((append_cell(l, i)) != true) errExit("append_Cell error");
-    } 
+    for (int32_t i = 0; i < 5; i++) {
+        if ((append_cell(l, i)) != true)
+            errExit("append_Cell error");
+    }
     display_list(l);
 
     printf("delete_headcell x2\n");
-    for(int32_t i=0; i<2; i++){
-        
-        if ((delete_headcell(l)) != true) errExit("delete_headcell error");
-    } 
+    for (int32_t i = 0; i < 2; i++) {
+
+        if ((delete_headcell(l)) != true)
+            errExit("delete_headcell error");
+    }
     display_list(l);
 
     printf("append x4\n");
-    for(int32_t i=99; i<103; i++){
-        if ((append_cell(l, i)) != true) errExit("append_cell error");
-    } 
+    for (int32_t i = 99; i < 103; i++) {
+        if ((append_cell(l, i)) != true)
+            errExit("append_cell error");
+    }
     display_list(l);
 
     printf("insert_cell(l, seek(l, 3), 999)\n");
-    if ((insert_cell(l, seek(l, 3), 999))!=true) errExit("insert_cell error!");
+    if ((insert_cell(l, seek(l, 3), 999)) != true)
+        errExit("insert_cell error!");
     display_list(l);
 
     printf("delete_tailcell x2\n");
-    for (int i=0; i<2; i++) {
+    for (int i = 0; i < 2; i++) {
         delete_cell(l, l->tail);
-        //if ((delete_tailcell(l)) != true) errExit("delete_tailcell");
+        // if ((delete_tailcell(l)) != true) errExit("delete_tailcell");
     }
     display_list(l);
 
     printf("delete_nextcell(l, seek(l, 1));\n");
-    if ((delete_nextcell(l, seek(l, 1))) != true) errExit("delete_nextcell");
+    if ((delete_nextcell(l, seek(l, 1))) != true)
+        errExit("delete_nextcell");
     display_list(l);
 
     printf("delete_nextcell(l, l->head);\n");
-    if ((delete_nextcell(l, l->head)) != true) errExit("delete_nextcell");
+    if ((delete_nextcell(l, l->head)) != true)
+        errExit("delete_nextcell");
     display_list(l);
 
     printf("\n\n----------------\n\n");
@@ -70,6 +75,7 @@ main(void)
     printf("\n\n----------------\n\n");
 
     printf("delete_list()\n");
-    if ((delete_list(l)) != true) errExit("delete_list");
+    if ((delete_list(l)) != true)
+        errExit("delete_list");
     puts("OK");
 }
