081182878b755b2fdf78b2e55c3943cfb451e8d8
tofu <tofu@wide.ad.jp>
2024-08-20 17:30:21 +0900

    add memo

--- 01_algo/liblinklist/liblinklist.h
+++ 01_algo/liblinklist/liblinklist.h
@@ -24,8 +24,17 @@ void display_cells(const Cell *head);
 Cell *cell_init(data_t v);
 // Cellの追加 (追加したCellのポインタを返す / cはNULLではいけない / Cはリスト末尾である必要がある) 
 Cell *cell_append(Cell *c, data_t v);
-// Todo : cとc->nextの間へのCell挿入
-// Cell *cell_insert(Cell *c, data_t v);
+/*
+Memo : 
+1:
+途中{のノードを削除, にノード挿入}する関数を実装すると
+ - 指定したアドレスの次のノードを削除する
+ - 指定したアドレスの次にノードを挿入する
+という仕様になり、これらは使いにくいので、実装がはしない
+
+2: 
+先頭または末尾のノード削除はやることが単純(freeするだけ)なのでAPIに追加しない
+*/
 // Cellの削除 (Cell->nextを返す)
 Cell *cell_delete(Cell *c);
 
