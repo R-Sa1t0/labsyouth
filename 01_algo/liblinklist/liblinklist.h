5b5c497c4a7e48a8bcc145da669c022545b3b345
tofu <tofu@wide.ad.jp>
2024-08-20 17:31:27 +0900

    delete struct list and apis for list

--- 01_algo/liblinklist/liblinklist.h
+++ 01_algo/liblinklist/liblinklist.h
@@ -12,11 +12,6 @@ typedef struct node {
   data_t data;
   struct node *next;
 } Cell;
-typedef struct list {
-  Cell *head;
-  Cell *tail;
-  size_t n;
-} List;
 
 // Cellの表示
 void display_cells(const Cell *head);
@@ -37,18 +32,3 @@ Memo :
 */
 // Cellの削除 (Cell->nextを返す)
 Cell *cell_delete(Cell *c);
-
-// Listの表示
-bool display_list(const List *l);
-// Listの初期化
-List *init_list();
-// Listの削除
-bool delete_list(List *l);
-// List内のn番目のポインタの取得 (nは0から)
-Cell *seek(List *l, size_t n);
-// Listへ要素追加
-bool list_append(List *l, data_t v);
-// Listへ要素挿入
-bool list_insert(List *l, Cell *c, data_t v);
-// Listの要素削除
-bool list_delete_cell(List *l, Cell *c);
--- 01_algo/liblinklist/linklist_test.c
+++ 01_algo/liblinklist/linklist_test.c
@@ -11,45 +11,5 @@ void errExit(const char *msg) {
 void warn(const char *msg) { printf("WARN: %s\n", msg); }
 
 int main(void) {
-  printf("init\n");
-  List *l = init_list();
-  if (l == NULL)
-    exit(1);
-  display_list(l);
-
-  printf("append x5\n");
-  for (int32_t i = 0; i < 5; i++) {
-    if ((list_append(l, i)) != true)
-      errExit("append_Cell error");
-  }
-  display_list(l);
-
-  printf("append x4\n");
-  for (int32_t i = 99; i < 103; i++) {
-    if ((list_append(l, i)) != true)
-      errExit("list_append error");
-  }
-  display_list(l);
-
-  printf("list_insert(l, seek(l, 3), 999)\n");
-  if ((list_insert(l, seek(l, 3), 999)) != true)
-    errExit("list_insert error!");
-  display_list(l);
-
-  printf("delete_tailcell x2\n");
-  for (int i = 0; i < 2; i++) {
-    list_delete_cell(l, l->tail);
-    // if ((delete_tailcell(l)) != true) errExit("delete_tailcell");
-  }
-  display_list(l);
-
-  printf("\n\n----------------\n\n");
-  list_delete_cell(l, l->head);
-  display_list(l);
-  printf("\n\n----------------\n\n");
-
-  printf("delete_list()\n");
-  if ((delete_list(l)) != true)
-    errExit("delete_list");
-  puts("OK");
+  puts("Hello!");
 }
