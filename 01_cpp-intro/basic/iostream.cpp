353e48009e26fb9ee38e6f007cef0998dab20bbe
tofu <tofu@wide.ad.jp>
2024-09-18 13:04:06 +0900

    add iostream

---
+++ 01_cpp-intro/basic/iostream.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "hello!!" << std::endl;
+  std::cout << "hello!!" << "World!!" << std::endl;
+  std::cout << 2525 << std::endl;
+
+  std::printf("Hello!");
+  std::printf("Hello!");
+  // commnent
+  /*
+    multi-line comment
+  */
+}
