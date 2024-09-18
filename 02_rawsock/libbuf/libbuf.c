125eb2bc709d7777cd2b80055c6fe143630d12d6
tofu <tofu@wide.ad.jp>
2024-09-18 13:56:50 +0900

    libbuf

---
+++ 02_rawsock/libbuf/libbuf.c
@@ -0,0 +1,32 @@
+#include "libbuf.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool print_buf(Buf *b) {
+  if (b == NULL)
+    return false;
+  fprintf(stdout, "len: %u, ", b->len);
+  fprintf(stdout, "next: %p, ", b->next);
+  for (int i = 0; i < BUF_SIZE; i++)
+    fprintf(stdout, "%02x ", b->v[i]);
+  puts("");
+  return false;
+}
+
+Buf *buf_init() {
+  Buf *new_buf = (Buf *)malloc(sizeof(Buf));
+  if (new_buf == NULL)
+    return NULL;
+  new_buf->next = NULL;
+  new_buf->len = 0;
+  memset(new_buf->v, 0, sizeof(new_buf->v));
+  return new_buf;
+}
+
+bool push() { return false; }
+
+bool pop() { return false; }
