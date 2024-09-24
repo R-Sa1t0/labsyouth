282562765219e805bbbe1024d3f1f7c697d45f01
tofu <tofu@wide.ad.jp>
2024-09-24 11:05:26 +0900

    add cpp_ringbuf

---
+++ 01_algo/async/cpp_ringbuf/main.cpp
@@ -0,0 +1,60 @@
+#include <condition_variable>
+#include <deque>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+// ringbuf
+struct ringbuf {
+  std::deque<int> deq;
+  std::mutex g_mtx;
+  std::condition_variable cv;
+  size_t ringbuf_cap;
+
+  // 初期化
+  ringbuf(size_t cap) : ringbuf_cap(cap) {}
+
+  void push(int v) {
+    {
+      std::unique_lock<std::mutex> lock(g_mtx);
+      cv.wait(lock, [this] { return deq.size() < ringbuf_cap; });
+      deq.push_back(v);
+    }
+    cv.notify_all();
+  }
+  int pop() {
+    std::unique_lock<std::mutex> lock(g_mtx);
+    cv.wait(lock, [this] { return !deq.empty(); });
+    int v = deq.front();
+    deq.pop_front();
+    cv.notify_all();
+    return v;
+  }
+};
+
+ringbuf rbuf(5);
+
+void producer() {
+  puts("producer thread start.");
+
+  for (int i = 0; i < 44; i++)
+    rbuf.push(i);
+
+  puts("producer thread end.");
+}
+void consumer() {
+  puts("consumer thread start.");
+
+  while (true) {
+    int v = rbuf.pop();
+    std::printf("%d\n", v);
+  }
+}
+
+int main() {
+  std::thread th_p(producer);
+  std::thread th_c(consumer);
+
+  th_p.join();
+  th_c.join();
+}
