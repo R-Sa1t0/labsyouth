aa76eeccae211847e24eb7701e9c7cdbc1949bd8
tofu <tofu@wide.ad.jp>
2024-10-01 14:38:29 +0900

    upd

--- 01_algo/async/cpp_ringbuf/main.cpp
+++ 01_algo/async/cpp_ringbuf/main.cpp
@@ -5,25 +5,27 @@
 #include <thread>
 
 // ringbuf
-struct ringbuf {
+class ringbuf {
+private:
   std::deque<int> deq;
-  std::mutex g_mtx;
+  std::mutex mtx;
   std::condition_variable cv;
   size_t ringbuf_cap;
 
+public:
   // 初期化
   ringbuf(size_t cap) : ringbuf_cap(cap) {}
 
   void push(int v) {
     {
-      std::unique_lock<std::mutex> lock(g_mtx);
+      std::unique_lock<std::mutex> lock(mtx);
       cv.wait(lock, [this] { return deq.size() < ringbuf_cap; });
       deq.push_back(v);
     }
     cv.notify_all();
   }
   int pop() {
-    std::unique_lock<std::mutex> lock(g_mtx);
+    std::unique_lock<std::mutex> lock(mtx);
     cv.wait(lock, [this] { return !deq.empty(); });
     int v = deq.front();
     deq.pop_front();
