ee8a6abf5508a5944ecdfe2c45e0f7ba974b7cf8
Raku Saito <tofu@sfc.wide.ad.jp>
2025-03-27 16:55:31 +0900

    refactor: 04_xdp/09_srv6_xdp_encap

--- 04_xdp/09_srv6_xdp_encap/set_map.py
+++ 04_xdp/09_srv6_xdp_encap/set_map.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     parser.add_argument("--saddr", required=True)
     parser.add_argument("--daddr", required=True)
     parser.add_argument("--sid", required=True)
-    parser.add_argument("--decapsid", required=True)
     args = parser.parse_args()
 
     smac_bytes = mac_to_bytes(args.smac)
@@ -25,16 +24,14 @@ if __name__ == "__main__":
     saddr_bytes = socket.inet_pton(socket.AF_INET6, args.saddr)
     daddr_bytes = socket.inet_pton(socket.AF_INET6, args.daddr)
     sid_bytes = socket.inet_pton(socket.AF_INET6, args.sid)
-    decapsid_bytes = socket.inet_pton(socket.AF_INET6, args.decapsid)
-    packed_value = struct.pack("<II6s6s16s16s16s16s",
+    packed_value = struct.pack("<II6s6s16s16s16s",
                                args.ifidx_wan,
                                args.ifidx_lan,
                                smac_bytes,
                                dmac_bytes,
                                saddr_bytes,
                                daddr_bytes,
-                               sid_bytes,
-                               decapsid_bytes)
+                               sid_bytes)
     packed_key = struct.pack("B", 0)
     key_hex = packed_key.hex()
     value_hex = " ".join(f"{b:02x}" for b in packed_value)
